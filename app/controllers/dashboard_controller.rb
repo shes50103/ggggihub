ec34a8c7a64c3e3ac476ad3e04c34d561086bf54
shes50103 <shes50103@gmail.com>
2020-04-30 20:00:39 +0800

    大概做出目錄、檔案系統

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,39 @@
+class DashboardController < ApplicationController
+
+  before_action :set_path
+
+  def index
+    @file_data = File.readlines(@path)
+    render :file
+
+  # 如果檔名無法開啟，就用目錄開啟
+  rescue Errno::EISDIR
+    Dir.entries(@path)
+    set_directory
+    render :index
+  end
+
+
+  private
+
+  def set_directory
+    @files = []
+    @dirs = []
+
+    # 設定目錄
+    Dir.entries(@path).each do |dir|
+      @dirs << "#{@path}/#{dir}" unless File.file?(dir)
+    end
+
+    # 設定檔案
+    Dir.entries(@path).each do |file|
+      @files << file if File.file?(file)
+    end
+  end
+
+  def set_path
+    @path = request.original_fullpath
+    @path[0] = ''
+    @path = '.' if @path == ''
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # root :to => 'dashboard#index'
+  get "/", to: "dashboard#index"
+  get "/:file", to: "dashboard#index"
+  get "/:file/:file", to: "dashboard#index"
+  get "/:file/:file/:file", to: "dashboard#index"
+  get "/:file/:file/:file/:file", to: "dashboard#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
